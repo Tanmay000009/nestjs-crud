ec40c0741c51b5518e0eb63efaddd87232e034a4
Tanmay000009 <tanmayvyas009@gmail.com>
2022-06-23 14:07:57 +0530

    add and setup nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, BookmarkModule, UserModule, PrismaModule],
+  imports: [
+    AuthModule,
+    BookmarkModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
