23478c77a31e7626702cfcafe51b8a046af5b012
Tanmay000009 <tanmayvyas009@gmail.com>
2022-06-23 17:30:35 +0530

    move gaurd to controller level

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,9 +3,9 @@ import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGaurd } from 'src/auth/gaurd';
 
+@UseGuards(JwtGaurd)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGaurd)
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
